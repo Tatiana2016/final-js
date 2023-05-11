a34b671fc2404372e44904b38b331699b9c14811
Tatiana2016 <serarfima@gmail.com>
2023-05-11 11:46:42 +0300

    partials

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/sections/header';
+import './js/sections/menu';
+import './js/sections/home';
+import './js/sections/category';
+import './js/sections/shopping-list';
+import './js/modal';
