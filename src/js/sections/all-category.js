65d81e3fb9e0a019a5c84461ec58bded8da7341c
Tatiana2016 <serarfima@gmail.com>
2023-05-13 20:05:49 +0300

    Part of styling

--- src/js/sections/all-category.js
+++ src/js/sections/all-category.js
@@ -5,13 +5,20 @@ const list = document.createElement('ul');
 const title = document.createElement('h2');
 
 title.textContent = 'All categories';
+title.classList.add('menu-item-light');
+list.classList.add('menu-category');
 list.style.listStyleType = 'none';
 container.append(title, list);
 
 async function renderCategories() {
   const response = await getBooksCategory();
   const categories = response.data;
-  const itemsHtml = categories.map(category => `<li>${category.list_name}</li>`).join('');
+  const itemsHtml = categories.map(category => 
+    `<a>
+     <li class="menu-item-light">${category.list_name}</li>
+     </a>`)
+      
+    .join('');
   list.insertAdjacentHTML('beforeend', itemsHtml);
 }
 
