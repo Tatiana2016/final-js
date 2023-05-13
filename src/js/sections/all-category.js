c40684c6c813fe14947ce6f754ee10a24fde119e
Tatiana2016 <serarfima@gmail.com>
2023-05-13 21:34:51 +0300

    working with conflict

--- src/js/sections/all-category.js
+++ src/js/sections/all-category.js
@@ -1,24 +1,24 @@
 import { getBooksCategory } from './getBooksCategories';
 
 const container = document.querySelector('.category-list');
-const list = document.createElement('ul');
 const title = document.createElement('h2');
+const list = document.createElement('ul');
+
 
 title.textContent = 'All categories';
 title.classList.add('menu-item-light');
 list.classList.add('menu-category');
+
 list.style.listStyleType = 'none';
 container.append(title, list);
 
 async function renderCategories() {
   const response = await getBooksCategory();
   const categories = response.data;
-  const itemsHtml = categories.map(category => 
-    `<a>
-     <li class="menu-item-light">${category.list_name}</li>
-     </a>`)
-      
-    .join('');
+  
+  const sortedCategories = categories.sort((a, b) => a.list_name.localeCompare(b.list_name));
+  const itemsHtml = sortedCategories.map(category => `<li class="menu-item-light">${category.list_name}</li>`).join('');
+
   list.insertAdjacentHTML('beforeend', itemsHtml);
 }
 
