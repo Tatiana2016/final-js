ea04c3dce842d10d3266b64c67251a4ad5f9dc5f
Tatiana2016 <serarfima@gmail.com>
2023-05-13 14:18:17 +0300

    category-list

--- src/js/sections/all-category.js
+++ src/js/sections/all-category.js
@@ -1,23 +1,18 @@
+import { getBooksCategory } from './getBooksCategories';
+
+const container = document.querySelector('.category-list');
 const list = document.createElement('ul');
 const title = document.createElement('h2');
+
 title.textContent = 'All categories';
 list.style.listStyleType = 'none';
+container.append(title, list);
 
-document.body.append(title, list);
-
-async function getBooksCategory() {
-  const BASE_URL = 'https://books-backend.p.goit.global/';
-  const ENDPOINT = 'books/category-list';
-
-  try {
-    const result = await axios.get(`${BASE_URL}${ENDPOINT}`);
-    console.log(result.data);
-
-    list.innerHTML = result.data.map(category => `<li>${category.list_name}</li>`).join('');
-
-  } catch (error) {
-    console.log(error);
-  }
+async function renderCategories() {
+  const response = await getBooksCategory();
+  const categories = response.data;
+  const itemsHtml = categories.map(category => `<li>${category.list_name}</li>`).join('');
+  list.insertAdjacentHTML('beforeend', itemsHtml);
 }
 
-getBooksCategory();
+renderCategories();
