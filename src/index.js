5eeb7c40c0a00ab6ab104bf66cc772a408be1259
Tatiana2016 <serarfima@gmail.com>
2023-05-15 18:15:53 +0300

    category

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './js/sections/books';
 import './js/sections/getBooksCategories';
 import './js/sections/getBooksByCategory';
 import './js/sections/getBookById';
-import './js/sections/renderBooks'
-import './js/sections/all-category'
+import './js/sections/renderBooks';
+import './js/sections/all-category';
 import './js/sections/getBooks';
 import './js/sections/getAllBooksByCategory';
 import './js/sections/createBooksMarkup';
@@ -15,6 +15,6 @@ import './js/sections/createAllBooksByCategoryMarkup';
 import './js/sections/category';
 import './js/sections/shopping-list';
 import './js/sections/support-ukraine';
-// import './js/modal';
+import './js/modal';
 import './js/sections/fetchApi';
 import './js/components/scroll-up';
--- src/js/sections/createAllBooksMarkup.js
+++ src/js/sections/createAllBooksMarkup.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
+//import axios from 'axios';
 import { getAllBooksByCategory } from './getAllBooksByCategory';
 import {createAllBooksByCategoryMarkup} from './createAllBooksByCategoryMarkup';
 import { getBooks } from './getBooks';
 
-const bookContainer = document.querySelector('.books-container');
+//const bookContainer = document.querySelector('.books-container');
 
 // Event listener to the document
-document.addEventListener('click', async function (event) {
-  const books = await getBooks()
-  const result = books.response.data;
-  if (event.target.classList.contains('.books-category-js')) {
-    event.preventDefault();
+//document.addEventListener('click', async function (event) {
+  //const books = await getBooks()
+  //const result = books.response.data;
+  //if (event.target.classList.contains('.books-category-js')) {
+    //event.preventDefault();
     // Category from the "data-category" attribute of the button
-    const category = event.target.dataset.category;
-    const response = await getAllBooksByCategory(category); // make API request for books
-    const allBooks = response.data;
-    bookContainer.innerHTML = createAllBooksByCategoryMarkup(allBooks, category);
-  }
-});
+    //const category = event.target.dataset.category;
+    //const response = await getAllBooksByCategory(category); // make API request for books
+    //const allBooks = response.data;
+    //bookContainer.innerHTML = createAllBooksByCategoryMarkup(allBooks, category);
+  //}
+//});
 
