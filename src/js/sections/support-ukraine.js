1d4bbc42ce207cc4411d76c4baaa0f7411ad625b
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-05-15 13:37:05 +0300

    update

--- src/js/sections/support-ukraine.js
+++ src/js/sections/support-ukraine.js
@@ -1,4 +1,6 @@
-import Swiper from 'swiper/swiper-bundle';
+// import Swiper from '../../../node_modules/swiper/swiper-bundle';
+import Swiper, { Navigation } from 'swiper';
+
 import 'swiper/swiper-bundle.min.css';
 import { supportList } from '../components/support-list';
 import { createImageItemMurkup } from './createMarkupSupportSection';
@@ -11,6 +13,7 @@ supportContainer.insertAdjacentHTML('beforeend', imageMarkup);
 swiperContainer = document.querySelector('.swiper-container');
 
 swiperOptions = {
+  modules: [Navigation],
   direction: 'vertical',
   slidesPerView: 4,
   spaceBetween: 20,
