3dbae6c235028bb66dce5efe14842802fa45d35b
Mariia2811 <mary.stupka11@gmail.com>
2023-05-18 14:34:08 +0300

    fixed scroll-up

--- src/js/components/scroll-up.js
+++ src/js/components/scroll-up.js
@@ -1,7 +1,7 @@
 const backToTopButton = document.getElementById("back-to-top-button");
 
 window.addEventListener("scroll", () => {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
         backToTopButton.style.display = "block";
     } else {
         backToTopButton.style.display = "none";
