9fb88c8b458764dd9dec259792c9b16e8c2bcd72
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-05-19 12:26:50 +0300

    see more частинка

--- src/js/sections/books.js
+++ src/js/sections/books.js
@@ -1,5 +1,5 @@
-import { getAllBooksByCategory } from './getAllBooksByCategory';
-import { createAllBooksByCategoryMarkup } from './createAllBooksByCategoryMarkup';
+// import { getAllBooksByCategory } from './getAllBooksByCategory';
+// import { createAllBooksByCategoryMarkup } from './createAllBooksByCategoryMarkup';
 import { getBooks } from './getBooks';
 import { createBooksMarkup } from './createBooksMarkup';
 
@@ -9,18 +9,19 @@ window.addEventListener('load', async () => {
   const response = await getBooks(); // make API request for books
   const books = response.data;
   const markup = createBooksMarkup(books); // create HTML markup
+  bookContainer.insertAdjacentHTML('beforeend', markup);
 });
 
-// See more button clicked - show 20 books
-document.addEventListener('click', async function (event) {
-  if (event.target.classList.contains('books-category-js')) {
-    event.preventDefault();
+// // See more button clicked - show 20 books
+// document.addEventListener('click', async function (event) {
+//   if (event.target.classList.contains('books-category-js')) {
+//     event.preventDefault();
 
-    const categoryId = event.target.getAttribute('data-category');
-    const books = await getAllBooksByCategory(categoryId);
-    const result = books.data;
+//     const categoryId = event.target.getAttribute('data-category');
+//     const books = await getAllBooksByCategory(categoryId);
+//     const result = books.data;
 
-    const markupCategory = createAllBooksByCategoryMarkup(result, categoryId);
-    bookContainer.innerHTML = markupCategory;
-  }
-});
+//     const markupCategory = createAllBooksByCategoryMarkup(result, categoryId);
+//     bookContainer.innerHTML = markupCategory;
+//   }
+// });
